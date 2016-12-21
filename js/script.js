ad812dfa32e920fc2f6d3ddf4d08c1bba1870b3a
Dominik Fiala <domo.fiala@gmail.com>
2016-12-21 21:52:10 +0100

    objekty misto poli pro snadnejsi pristup

--- js/script.js
+++ js/script.js
@@ -1,92 +1,142 @@
 var data = {
-  templates: [
-    {
-      name: 'stl-svetly',
+  templates: {
+    'stl-svetly': {
       title: 'St. Louis Blues hosté',
       style: {
         image: './gfx/stl-svetly.png'
       },
-      text: [
-        {
-          name: 'surname',
-          controls: {title: 'Příjmení', size: 9},
+      text: {
+        'surname': {
+          controls: {
+            placeholder: 'Příjmení',
+            parentClass: 'col-xs-9'
+          },
           style: {
+            'x': 150,
+            'y': 261,
+            'text-anchor': 'middle',
             'font-family': 'new_athletic_m54regular',
-            fill: '#00285d',
-            stroke: '#ffb924',
+            'fill': '#00285d',
+            'stroke': '#ffb924',
             'stroke-width': 4,
             'paint-order': 'stroke',
             'font-size': 34
           }
         },
-        {
-          name: 'number',
-          controls: {title: 'Číslo', size: 2},
+        'number': {
+          controls: {
+            placeholder: 'Číslo',
+            parentClass: 'col-xs-2'
+          },
           style: {
+            'x': 190,
+            'y': 105,
+            'text-anchor': 'middle',
             'font-family': 'new_athletic_m54regular',
-            fill: '#00285d',
-            stroke: '#ffb924',
+            'fill': '#00285d',
+            'stroke': '#ffb924',
             'stroke-width': 4,
             'paint-order': 'stroke',
             'font-size': 42
           }
         }
-      ]
-    }
-  ]
+      }
+    },
+    'stl-tmavy': {
+        title: 'St. Louis Blues domácí',
+        style: {
+          image: './gfx/stl-tmavy.png'
+        },
+        text: {
+          'surname': {
+            controls: {
+              placeholder: 'Příjmení',
+              parentClass: 'col-xs-9'
+            },
+            style: {
+              'x': 150,
+              'y': 261,
+              'text-anchor': 'middle',
+              'font-family': 'new_athletic_m54regular',
+              'fill': '#00285d',
+              'stroke': '#ffb924',
+              'stroke-width': 4,
+              'paint-order': 'stroke',
+              'font-size': 34
+            }
+          },
+          'number': {
+            controls: {
+              placeholder: 'Číslo',
+              parentClass: 'col-xs-2'
+            },
+            style: {
+              'x': 190,
+              'y': 105,
+              'text-anchor': 'middle',
+              'font-family': 'new_athletic_m54regular',
+              'fill': '#00285d',
+              'stroke': '#ffb924',
+              'stroke-width': 4,
+              'paint-order': 'stroke',
+              'font-size': 42
+            }
+          }
+        }
+      }
+  }
 };
 
 if (window.localStorage) {
   var store = window.localStorage
 
   if (store.getItem('groups')) {
+  // if (0 && store.getItem('groups')) {
     data.groups = JSON.parse(store.getItem('groups'));
   }
   else {
-    data.groups = [];
+    data.groups = {};
   }
 }
 
 var app = new Vue({
   el: '#app',
   data: data,
+  computed: {
+    availableTemplates: function() {
+      var out = {};
+      for (group in this.templates) {
+        if (!this.groups[group]) {
+          out[group] = this.templates[group].title;
+        }
+      }
+      return out;
+    }
+  },
   methods: {
     addGroup: function() {
       var option = document.getElementById("add-group-select");
-      var groupName = option.options[option.selectedIndex].value;
-      var group = this.templates.filter(function(template) {
-        if (template.name === groupName) return true;
-      })[0];
+      var group = option.options[option.selectedIndex].value;
       if (group) {
+        option.selectedIndex = 0;
         option.parentNode.classList.remove('has-error');
-        this.groups.unshift({
-          template: group,
-          items: []
-        });
+        this.$set(this.groups, group, []);
       }
       else {
         option.parentNode.classList.add('has-error');
         option.focus();
       }
     },
-    addItem: function(index) {
-      var group = this.groups.filter(function(group, groupIndex) {
-        if (groupIndex === index) return true;
-      })[0];
-      var fields = [];
-      for (text of group.template.text) {
-        field = {
-          value: '',
-          name: text.name,
-          title: text.controls.title,
-          class: 'col-xs-' + text.controls.size
-        };
-        fields.push(field);
+    addItem: function(groupIndex) {
+      var template = this.templates[groupIndex];
+      var item = {};
+      for (text in template.text) {
+        item[text] = '';
       }
-      group.items.unshift(fields);
+      this.groups[groupIndex].unshift(item);
     },
     removeItem: function(groupIndex, itemIndex) {
-      this.groups[groupIndex].items.splice(itemIndex, 1);
+      this.groups[groupIndex].splice(itemIndex, 1);
     },
     tabPressed: function(event) {
       console.log(event, this.addItem);
